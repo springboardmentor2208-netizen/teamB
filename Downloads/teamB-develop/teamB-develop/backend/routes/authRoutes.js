8814d04c02210cf80d79d609f206d4ded9e7b021
syedasad555 <syedasad5151@gmail.com>
2026-02-13 19:08:57 +0530

    Delete Downloads/teamB-develop/teamB-develop/backend directory

--- Downloads/teamB-develop/teamB-develop/backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginUser, registerUser } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/login", loginUser);
-router.post("/register", registerUser);
-
-export default router;
